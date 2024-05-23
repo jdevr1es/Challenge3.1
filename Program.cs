30096c3168cedf386c137e8306584802270748b9
John DeVries <johndevries49@gmail.com>
2024-05-23 11:24:35 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Challenge3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a word to check if it is a palindrome: ");
+            string myWord = Console.ReadLine();
+
+            if (TestPalindrome(myWord))
+            {
+                Console.WriteLine($"{myWord} is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"{myWord} is not a palindrome.");
+            }
+        }
+        static bool TestPalindrome(string palindrome)
+        {
+            bool IsPalindrome = false;
+            for (int i = 0; i < palindrome.Length / 2; i++)
+            {
+                if (!(palindrome[i] == palindrome[palindrome.Length - 1 - i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
